2a4613aa61c4e0d88e59ceb2437b24a5720bba00
Antoine Struelens <antoine.struelens@gmail.com>
2023-06-26 15:56:30 +0200

    add to local storage

--- exercise 2/script.js
+++ exercise 2/script.js
@@ -1,9 +1,11 @@
+//Import a JSON file with ISO list of countries available in the API.
 import countryList from "./countries.json" assert { type: "json" };
 
+//Declares constants for selectors
 const button = document.querySelector("button");
-
 const countrySelector = document.querySelector("select");
 
+//Populates the select element with all the country options
 countryList.forEach((country) => {
   let newOption = document.createElement("option");
   newOption.innerText = country.Country;
@@ -12,8 +14,19 @@ countryList.forEach((country) => {
   countrySelector.append(newOption);
 });
 
+//Add locally stored queries
+console.log(localStorage.length);
+for (let i = 1; i <= localStorage.length; i++) {
+  let oldEntry = document.createElement("div");
+  oldEntry.innerText = localStorage.getItem(`entry${i}`);
+  document.body.append(oldEntry);
+}
+
+//Fetches the object arguments from the API
 const fetchName = (name, country) =>
   fetch(`https://api.agify.io/?name=${name}&country_id=${country}`);
+
+//Fetches the arguments from the fetchName function and displays the average age and population for a specific name
 
 const fetchInfo = (name, country) => {
   fetchName(name, country)
@@ -34,6 +47,7 @@ const fetchInfo = (name, country) => {
           ].id
         }`;
         newLog.append(newEntry);
+        localStorage.setItem(`entry${localStorage.length + 1}`, newEntry);
         document.body.append(newLog);
       }
     })
@@ -42,8 +56,22 @@ const fetchInfo = (name, country) => {
     });
 };
 
+//Triggers the fetchInfo function when the button is clicked and passes the arguments based on the input and selected country
 button.addEventListener("click", (e) => {
   const nameInput = document.querySelector("input").value;
   const countrySelect = document.getElementById("country-select").value;
   fetchInfo(nameInput, countrySelect);
 });
+
+//Create a clear button next to the get name stats
+const clearButton = document.createElement("button");
+clearButton.innerText = "Clear";
+button.after(clearButton);
+
+//Clears the content of the local storage and refreshes the page
+clearButton.addEventListener("click", (e) => {
+  localStorage.clear();
+  location.reload();
+});
+
+console.log(localStorage);
