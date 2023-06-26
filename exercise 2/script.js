36561f3e26073c819306fbba83bd52fdc4590883
Antoine Struelens <antoine.struelens@gmail.com>
2023-06-26 12:24:14 +0200

    added all countries to select element via json file

--- exercise 2/script.js
+++ exercise 2/script.js
@@ -1,24 +1,49 @@
-const button = document.querySelector("button");
-const fetchName = (name) => fetch("https://api.agify.io/?name=" + name);
+import countryList from "./countries.json" assert { type: "json" };
 
-const fetchInfo = (name) => {
-  fetchName(name)
+const button = document.querySelector("button");
+
+const countrySelector = document.querySelector("select");
+
+countryList.forEach((country) => {
+  let newOption = document.createElement("option");
+  newOption.innerText = country.Country;
+  newOption.setAttribute("id", `${country.Country}`);
+  newOption.setAttribute("value", `${country["ISO Code"]}`);
+  countrySelector.append(newOption);
+});
+
+const fetchName = (name, country) =>
+  fetch(`https://api.agify.io/?name=${name}&country_id=${country}`);
+
+const fetchInfo = (name, country) => {
+  fetchName(name, country)
     .then((response) => response.json())
     .then((json) => {
-      if (json.age == null){
-        alert("the name " + name + " is not in the database")
-      } else{
-      let newLog = document.createElement("div");
-      let newEntry = `${json.name} are on average ${json.age} year old and there are ${json.count} of them`;
-      newLog.append(newEntry);
-      document.body.append(newLog);}
+      if (json.age == null) {
+        alert("the name " + name + " is not in the database");
+      } else {
+        console.log(json.country_id);
+
+        console.log(json.country_id);
+        let newLog = document.createElement("div");
+        let newEntry = `${json.name} are on average ${
+          json.age
+        } year old and there are ${json.count} of them in ${
+          document.querySelector("select").options[
+            document.querySelector("select").selectedIndex
+          ].id
+        }`;
+        newLog.append(newEntry);
+        document.body.append(newLog);
+      }
     })
     .catch((error) => {
-      alert("there seems to have been an "+error);
+      alert("there seems to have been an " + error);
     });
 };
 
 button.addEventListener("click", (e) => {
   const nameInput = document.querySelector("input").value;
-  fetchInfo(nameInput);
+  const countrySelect = document.getElementById("country-select").value;
+  fetchInfo(nameInput, countrySelect);
 });
